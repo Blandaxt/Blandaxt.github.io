ce0fc720114bf900b09494c7dff39df632b5f09f
Alain Lafume <alainsachievements@gmail.com>
2019-05-09 16:30:52 -0400

    Horoscope almost done

--- Horoscope/js/horo.js
+++ Horoscope/js/horo.js
@@ -6,14 +6,31 @@ function findHoro(){
 // get value of day
 var day = document.getElementsByTagName("select")[1].value
 
+// Setting all of the blocks to be style.display = to none when clicked and only selecting the one that has the properdate as the picture to show.
+document.getElementsByClassName("hide")[0].style.display = "none";
+document.getElementsByClassName("hide")[1].style.display = "none";
+document.getElementsByClassName("hide")[2].style.display = "none";
+document.getElementsByClassName("hide")[3].style.display = "none";
+document.getElementsByClassName("hide")[4].style.display = "none";
+document.getElementsByClassName("hide")[5].style.display = "none";
+document.getElementsByClassName("hide")[6].style.display = "none";
+document.getElementsByClassName("hide")[7].style.display = "none";
+document.getElementsByClassName("hide")[8].style.display = "none";
+document.getElementsByClassName("hide")[9].style.display = "none";
+document.getElementsByClassName("hide")[10].style.display = "none";
+document.getElementsByClassName("hide")[11].style.display = "none";
+// Another way is to use Aray.from(class goes here).forEach( element){
+//element.style.display = "none"
+// })}
+
 // Aquarius..................        ........
 // function of the day
 if( (month ==1 && day >=1) || (month == 2 && day <=20)){
   //var horo = document.getElementById("aquarius").value
 // get the element by tag name and display it
-  document.getElementsByTagName("p")[10].style.display = "block";
+   document.getElementsByClassName("hide")[10].style.display = "block";
 // shows activity in the log
-  console.log(aquarius);
+  console.log("My Horoscope is: aquarius");
 
 // elst this will disapear
 }
@@ -28,9 +45,12 @@ if( (month ==1 && day >=1) || (month == 2 && day <=20)){
 // show the other month
 else if( (month ==2 && day >=21) || (month ==3 && day <=20) ){
 
-  document.getElementsByTagName("p")[11].style.display = "block";
+   // document.getElementsByTagName("p")[11].style.display = "block";
 
-  console.log(pisces);
+   document.getElementsByClassName("hide")[11].style.display = "block"
+
+
+  console.log("My Horoscope is: pisces");
 
 }
 
@@ -38,90 +58,116 @@ else if( (month ==2 && day >=21) || (month ==3 && day <=20) ){
 
 else if ( (month ==3 && day >=21) || (month ==4 && day <=20) ){
 
-  document.getElementsByTagName("p")[1].style.display = "block";
+   // document.getElementsByTagName("p")[1].style.display = "block";
 
-  console.log(aries);
+    document.getElementsByClassName("hide")[1].style.display = "block"
+
+  console.log("My Horoscope is: aries");
 }
 
 // taurus.........................
 
 else if ( (month ==4 && day >=21) || (month ==5 && day <=21) ){
 
-  document.getElementsByTagName("p")[0].style.display = "block";
+  // document.getElementsByTagName("p")[0].style.display = "block";
+  document.getElementsByClassName("hide")[0].style.display = "block"
 
-  console.log(taurus);
+  console.log("My Horoscope is: taurus");
 }
 
 // gemini... ..  . . . . . . . ..    ..
 
 else if ( (month ==5 && day >=22) || (month ==6 && day <=21) ){
 
-  document.getElementsByTagName("p")[2].style.display = "block";
+  // document.getElementsByTagName("p")[2].style.display = "block";
 
-  console.log(gemini);
+    document.getElementsByClassName("hide")[2].style.display = "block"
+
+  console.log("My Horoscope is: gemini");
 }
 
 // cancer      .   . .   . . . . ..  . .
 
 else if ( (month ==6 && day >=22) || (month ==7 && day <=22) ){
 
-  document.getElementsByTagName("p")[3].style.display = "block";
+  // document.getElementsByTagName("p")[3].style.display = "block";
 
-  console.log(cancer);
+  document.getElementsByClassName("hide")[3].style.display = "block"
+
+
+  console.log("My Horoscope is: cancer");
 }
 
 // leo.. ... ..    . . . . ....
 
 else if ( (month ==7 && day >=23) || (month ==8 && day <=21) ){
 
-  document.getElementsByTagName("p")[4].style.display = "block";
+  // document.getElementsByTagName("p")[4].style.display = "block";
 
-  console.log(leo);
+  document.getElementsByClassName("hide")[4].style.display = "block"
+
+
+  console.log("My Horoscope is: leo");
 }
 
 // virgo...... ..    ..  . . ..
 
 else if ( (month ==8 && day >=22) || (month ==9 && day <=23) ){
 
-  document.getElementsByTagName("p")[5].style.display = "block";
+  // document.getElementsByTagName("p")[5].style.display = "block";
 
-  console.log(virgo);
+  document.getElementsByClassName("hide")[5].style.display = "block"
+
+
+  console.log("My Horoscope is: virgo");
 }
 
 // libra.... ..  . ..  . . . . ..
 
 else if ( (month ==9 && day >=24) || (month ==10 && day <=23) ){
 
-  document.getElementsByTagName("p")[6].style.display = "block";
+  // document.getElementsByTagName("p")[6].style.display = "block";
 
-  console.log(libra);
+  document.getElementsByClassName("hide")[6].style.display = "block"
+
+
+  console.log("My Horoscope is: libra");
 }
 
 // scorpio.... ..  . ..  . . ..
 
 else if ( (month ==10 && day >=24) || (month ==11 && day <=22) ){
 
-  document.getElementsByTagName("p")[7].style.display = "block";
+  // document.getElementsByTagName("p")[7].style.display = "block";
 
-  console.log(scorpio);
+  document.getElementsByClassName("hide")[7].style.display = "block"
+
+
+  console.log("My Horoscope is: scorpio");
 }
 
 // sagittarius...  . . . . . . . . . . ....
 
 else if ( (month ==11 && day >=23) || (month ==12 && day <=22) ){
 
-  document.getElementsByTagName("p")[8].style.display = "block";
+  // document.getElementsByTagName("p")[8].style.display = "block";
 
-  console.log(sagittarius);
+  document.getElementsByClassName("hide")[8].style.display = "block"
+
+
+  console.log("My Horoscope is: sagittarius");
 }
 
 // capricorn... . . . . . . . . . . .
 
 else if ( (month ==12 && day >=23) || (month ==1 && day <=20) ){
 
-  document.getElementsByTagName("p")[9].style.display = "block";
+  // document.getElementsByTagName("p")[9].style.display = "block";
 
-  console.log(capricorn);
+  document.getElementsByClassName("hide")[9].style.display = "block"
+
+
+  console.log("My Horoscope is: capricorn");
 }
 
 }
